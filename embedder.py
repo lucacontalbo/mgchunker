520e371e7374cbe4cb74935d476cc5a8cb0ea8c2
lucacontalbo <luca.contalbo@gmail.com>
2025-08-27 15:54:46 +0200

    modified faiss index to gpu

--- embedder.py
+++ embedder.py
@@ -12,26 +12,39 @@ import os
 import warnings
 from multiprocessing import cpu_count
 import time
+from sentence_transformers import SentenceTransformer
 
 warnings.filterwarnings("ignore")
 faiss.omp_set_num_threads(int(os.environ.get("SLURM_CPUS_PER_TASK", cpu_count())))
 print(f"Using {faiss.omp_get_max_threads()} instead of value given by cpu_count: {cpu_count()}")
 
-cls = ["sup-simcse-bert-base-uncased", "dpr-ctx_encoder-multiset-base"]
-mean_pooling = ["gtr-t5-base", "contriever", "multilingual-e5-large-instruct"]
+pooler_output = ["dpr-ctx_encoder-multiset-base"]
+cls = ["sup-simcse-bert-base-uncased"]
+st = ["gtr-t5-base"] #sentencetransformer
+mean_pooling = ["contriever", "multilingual-e5-large-instruct"]
 
-def get_embedding(texts: List[str], tokenizer, model, model_name) -> np.ndarray:
-    encoded = tokenizer(texts, padding=True, truncation=True, return_tensors='pt').to("cuda")
+def get_embedding(texts: List[str], model_name, tokenizer=None, model=None) -> np.ndarray:
+    if tokenizer is not None:
+        encoded = tokenizer(texts, padding=True, truncation=True, return_tensors='pt').to("cuda")
+
     with torch.no_grad():
-        if hasattr(model, "encoder"):
-            token_embs = model.encoder(input_ids=encoded["input_ids"], attention_mask=encoded["attention_mask"]).last_hidden_state
+        if model_name in st:
+            token_embs = model.encode(texts)
+        elif hasattr(model, "encoder"):
+            token_embs = model.encoder(input_ids=encoded["input_ids"], attention_mask=encoded["attention_mask"])
         else:
-            token_embs = model(**encoded).last_hidden_state
+            token_embs = model(**encoded)
 
         if model_name in mean_pooling:
+            token_embs = token_embs.last_hidden_state
             mask = encoded["attention_mask"].unsqueeze(-1).to(token_embs.dtype)
             sent_embs = (token_embs * mask).sum(dim=1) / mask.sum(dim=1).clamp_min(1e-9)
+        elif model_name in pooler_output:
+            sent_embs = token_embs.pooler_output
+        elif model_name in st:
+            pass
         else:
+            token_embs = token_embs.last_hidden_state
             sent_embs = token_embs[:,0] #cls
         sent_embs = F.normalize(sent_embs, p=2, dim=1)
 
@@ -46,12 +59,17 @@ def main():
     parser.add_argument('--nlist', type=int, default=1024)
     parser.add_argument('--m', type=int, default=96)
     parser.add_argument('--batch_size', type=int, default=1024)
-    parser.add_argument('--max_train_samples', type=int, default=3_000_000)
+    parser.add_argument('--max_train_samples', type=int, default=300_000)
     args = parser.parse_args()
 
     print("Start!")
-    tokenizer = AutoTokenizer.from_pretrained(args.model)
-    model = AutoModel.from_pretrained(args.model).to("cuda")
+    model_name = args.model.split("/")[-1]
+    if model_name in st:
+        tokenizer = None
+        model = SentenceTransformer(args.model).to("cuda")
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(args.model)
+        model = AutoModel.from_pretrained(args.model).to("cuda")
     model.eval()
 
     dim = None
@@ -62,7 +80,7 @@ def main():
     total_seen = 0
     for chunk in tqdm(reader, desc="Training sample collection"):
         texts = chunk["contents"].tolist()
-        embs = get_embedding(texts, tokenizer, model, args.model.split("/")[-1])
+        embs = get_embedding(texts, model_name, tokenizer, model)
         if dim is None:
             dim = embs.shape[1]
         train_embeddings.append(embs)
@@ -79,6 +97,9 @@ def main():
     quantizer = faiss.IndexFlatIP(dim)
     index = faiss.IndexIVFPQ(quantizer, dim, args.nlist, args.m, 16)
     index.metric_type = faiss.METRIC_INNER_PRODUCT
+    res = faiss.StandardGpuResources()
+    index = faiss.index_cpu_to_gpu(res, 0, index)
+
     index.train(train_matrix)
 
     print("🔄 Second pass: Adding full dataset to the index...")
@@ -89,7 +110,7 @@ def main():
     for chunk in tqdm(reader, desc="Adding to index"):
         texts = chunk["contents"].tolist()
         string_ids = chunk["id"].tolist()
-        embs = get_embedding(texts, tokenizer, model)
+        embs = get_embedding(texts, model_name, tokenizer, model)
 
         num_vectors = len(embs)
         faiss_ids = np.arange(current_id, current_id + num_vectors, dtype=np.int64)
@@ -110,7 +131,7 @@ def main():
             with open(args.output_meta, "w") as f:
                 json.dump(metadata, f, indent=2)
 
-
+    index = faiss.index_gpu_to_cpu(index)
     faiss.write_index(index, args.output_index)
     with open(args.output_meta, "w") as f:
         json.dump(metadata, f, indent=2)
